4f344c806563aca7aa04f4e742e6602be4fecca7
Miss-you <snowfly1993@gmail.com>
2016-04-03 00:24:08 +0800

    rm passwd

--- xvmGET.py
+++ xvmGET.py
@@ -1,30 +1,3 @@
-#import urllib2
-#import urllib
-
-
-#response=urllib2.urlopen('http://182.18.61.50/search.html')
-#urlmsg=response.read()
-#print urlmsg
-#response=urllib2.urlopen('http://blog.csdn.net/qq_15437667/article/details/50957996')
-
-
-#values = {"username":"794089112@qq.com","password":"lhy19970512"}
-#data = urllib.urlencode(values) 
-#url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
-#request = urllib2.Request(url,data)
-#response = urllib2.urlopen(request)
-#print response.read()
-
-#url = 'http://www.douban.com'
-#info = {'name' : 'Michael Foord',
-#          'location' : 'Northampton'}
-#data = urllib.urlencode(info)
-#req = urllib2.Request(url, data)
-#response = urllib2.urlopen(req)
-#the_page = response.read()
-#print the_page
-
-
 import re
 import urllib
 
