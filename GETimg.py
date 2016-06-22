e20ddb374eff93b484a4b3a7b1006a80d7dcae27
Miss-you <snowfly1993@gmail.com>
2016-06-23 00:14:18 +0800

    down img by group

--- GETimg.py
+++ GETimg.py
@@ -4,27 +4,14 @@ import urllib
 import urllib2
 import re
 import os
+import shutil
   
-#Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13
-#Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25 
-
-#61.172.249.96:80
-
-
-
 def getImg(html):
-    #reg = r'"http://ww[0-9].sinaimg.cn/large/(.+?\.jpg)"'
-    reg = r'http://ww[0-9].sinaimg.cn/large/.+?\.jpg'
+    #http://ww2.sinaimg.cn/large/d030806ajw1f53845zoo3j20my0g1di1.jpg 
+    reg = r'http://ww[0-9].sinaimg.cn/.+?\.jpg'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
     return imglist 
-#http://ww2.sinaimg.cn/large/d030806ajw1f53845zoo3j20my0g1di1.jpg
-    
-def getArticleHtml(html):
-    reg = r'[3-9][0-9]{7}'
-    htmlre = re.compile(reg)
-    arthtmllist = re.findall(htmlre,html)
-    return arthtmllist     
 
 #获取网页信息
 def getHtml(url, values, headers):
@@ -50,6 +37,7 @@ def getHtml(url, values, headers):
     
     return page   
 
+#保存文件
 def saveFile(target, data):
     if data == None:
         return
@@ -59,75 +47,65 @@ def saveFile(target, data):
     file.flush()
     file.close()
 
-def downImg(html_imgList):
-    picPath = '/Users/yousa/Desktop/pic'
+#根据照片连接下载照片
+def downImg(html_imgList, imgSerial):
+    picPath = '/Users/yousa/Desktop/pic/' + str(imgSerial)
+    
+    if len(html_imgList) == 0:
+        print ("html_imgList is None")
+        return
+    
+    #清理过去图片
+    if os.path.exists(picPath):
+        shutil.rmtree(picPath)
+        
+    os.makedirs(picPath)  
+     
     imgNo = 1
     
-    #if not os.path.exists(picPath):
-    #    os.makedirs(picPath) 
-        
     for imgurl in html_imgList:
-        print imgurl
+        #print imgurl
         filename = str(imgNo) + '.jpg'
         imgNo = imgNo + 1
         target = picPath + '/' + filename
-        #urllib.urlretrieve(imgurl, target)
         saveFile(target, getHtml(imgurl, None, headers))
           
     
 #url = 'http://www.jdlingyu.com/'
-url = 'http://www.jdlingyu.com/11894/'
+#11992
+url = 'http://www.jdlingyu.com/'
 user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25'
 headers = { 'User-Agent' : user_agent }
-#user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'  
 
-#enable proxy
+def downloadHtmlImg(imgSerial):
+    imgUrl = url + str(imgSerial)
+    print imgUrl
+    
+    #get page
+    page = getHtml(imgUrl, None, headers)
+    if page == None:
+        print(imgUrl + "unavailable!")
+        return
+        
+    #get img list
+    imglist = getImg(page)
+    imglist = list(set(imglist))
+    #download img
+    downImg(imglist, imgSerial)
+    
+#for i = 0; i < 5; i++:
+def run():
+    for i in range(0, 25):
+        #print i
+        downloadHtmlImg(11992 - i)
 
+run()
 
-#use safari Agent
-#page = getHtml(url, values, headers)
-
-
-
-#save_file('/Users/yousa/Desktop/pic', '1.jpg', getHtml('http://ww4.sinaimg.cn/large/d030806ajw1f538470z1gj22bc3h0476.jpg', None, headers)) 
-
-   
-
+'''
 page = getHtml(url, None, headers)
 
 imglist = getImg(page)
 print imglist
 
 downImg(imglist)
-
-
-'''
-
-# -*- coding:utf-8 -*-
-import urllib
-import urllib2
-import re
- 
-page = 1
-url = 'http://www.qiushibaike.com/hot/page/' + str(page)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
-try:
-    request = urllib2.Request(url,headers = headers)
-    response = urllib2.urlopen(request)
-    content = response.read().decode('utf-8')
-</\>    print content
-##<div class="mlr mt10 content-text">([\u000d\u000a\u4e00-\u9fa5\uff00-\uff20\u3002]+)<\/div>
-    pattern = re.compile('<div.*?class="mlr.*?mt10.*?content-text>.*?([\u000d\u000a\u4e00-\u9fa5\uff00-\uff20\u3000-\u3010]+).*?<\/div>',re.S)
-    items = re.findall(pattern,content)
-    for item in items:
-        haveImg = re.search("img",item[3])
-        if not haveImg:
-            print item[0],item[1],item[2],item[4]
-except urllib2.URLError, e:
-    if hasattr(e,"code"):
-        print e.code
-    if hasattr(e,"reason"):
-        print e.reason
-  
 '''
